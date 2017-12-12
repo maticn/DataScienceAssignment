4707042f61431df5aa6fba82a57be080766f0c88
maticn <maticnova@gmail.com>
2017-12-12 13:40:51 +0100

    Ploting Boston.

--- 2_LDA/boston.R
+++ 2_LDA/boston.R
@@ -45,7 +45,7 @@ lr_model_all <- train(Class~., data=trainingData,
                       method='glm', family=binomial(link='logit'),
                       preProcess=c('scale', 'center'))
 lr_pred_all <- predict(lr_model_all, testData[,-14])
-confusionMatrix(table(lr_pred_all, testData$Class))
+confusionMatrix(lr_pred_all, testData$Class)
 
 lr_model_corr <- train(Class ~ rad + nox,
                        data=trainingData, 
@@ -81,4 +81,23 @@ knn_model_corr <- train(x=trainingDataCorr[,-12], method='knn',
              preProcess=c('center', 'scale'), 
              tuneGrid = knnGrid)
 knn_pred <- predict(knn_model_all, testData[,-14])
-confusionMatrix(table(knn_pred, testLabels))
+confusionMatrix(knn_pred, testLabels)
+
+
+# Plot the odds ratios and boundaries for logistic regression.
+betasBac <- getBetas(summary(lr_model_all))
+oddsRatiosBac <- getOddsRatios(betasBac)
+standardErrorsBac <- getStandardErrors(lr_model_all)
+boundsBac <- getBounds(oddsRatiosBac, betasBac, standardErrorsBac)
+boundsBac<-boundsBac[!(boundsBac$OR > 10000),]
+
+dev.off()
+boxLabels = c("zn", "indus", "chas", "rm", "age", "dis", "rad", "tax", "ptratio", "black", "lstat", "medv")
+yAxis = length(boxLabels):1
+df <- data.frame(
+  yAxis = length(boxLabels):1,
+  boxOdds = c(boundsBac[2,1],boundsBac[3,1],boundsBac[4,1],boundsBac[5,1],boundsBac[6,1],boundsBac[7,1],boundsBac[8,1],boundsBac[9,1],boundsBac[10,1],boundsBac[11,1],boundsBac[12,1],boundsBac[13,1]),
+  boxCILow = c(boundsBac[2,2],boundsBac[3,2],boundsBac[4,2],boundsBac[5,2],boundsBac[6,2],boundsBac[7,2],boundsBac[8,2],boundsBac[9,2],boundsBac[10,2],boundsBac[11,2],boundsBac[12,2],boundsBac[13,2]),
+  boxCIHigh = c(boundsBac[2,3],boundsBac[3,3],boundsBac[4,3],boundsBac[5,3],boundsBac[6,3],boundsBac[7,3],boundsBac[8,3],boundsBac[9,3],boundsBac[10,3],boundsBac[11,3],boundsBac[12,3],boundsBac[13,3])
+)
+forestPlot(df, boxLabels, yAxis, "Models")
